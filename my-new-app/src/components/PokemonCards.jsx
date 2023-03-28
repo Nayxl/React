d4d0457490cecd64e2d833ed7d6f87cab4c5eee7
Aylin Noiret <aylinndream@hotmail.fr>
2023-03-28 11:18:47 +0200

    React basics 03: Afficher des données dans un composant

--- my-new-app/src/components/PokemonCards.jsx
+++ my-new-app/src/components/PokemonCards.jsx
@@ -1,11 +1,24 @@
 function PokemonCards() {
+    const pokemon = pokemonList[0];
     return (
         <figure> 
             <h1>PokemonCards</h1>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img> 
-            <figcaption> Bulbaseur </figcaption>
+            {pokemon.imgSrc === undefined ? <p>???</p> : 
+            <img src={pokemon.imgSrc} alt={pokemon.name}></img> }
+            <figcaption> {pokemon.name} </figcaption>
         </figure>
     )
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCards;

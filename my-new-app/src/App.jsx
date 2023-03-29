7f1ceeef94244eb71a609a4d9b2b023e7440a3d0
Aylin Noiret <aylinndream@hotmail.fr>
2023-03-29 11:17:56 +0200

    React Basics 06: Utiliser un state

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -5,26 +5,58 @@ import './App.css'
 import './MyTitle'
 import PokemonCards from './components/PokemonCards'
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
+
+  const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
 
 function App() {
-  const [count, setCount] = useState(0)
-  let pokemon = pokemonList[0];
+  const [index, setIndex] = useState(0)
+  let pokemon = pokemonList[index];
+  const previousPokemon = () => {
+    if (index > 0){
+      setIndex(index-1);
+    }
+  };
+  const nextPokemon = () => {
+    if (index < pokemonList.length - 1){
+      setIndex(index+1);
+    }
+  }
+
   return (
     <div className="App">
       
        <PokemonCards name={pokemon.name} imgSrc={pokemon.imgSrc}/>
+    <button onClick={previousPokemon}>Précédent</button>
+    <button onClick={nextPokemon}>Suivant</button>
     </div>
   )
 }
 
+
 export default App
+
